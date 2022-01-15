baa49f2071371bad5c5176adeebd71d1c6627032
SirajumMahdi <hmahdy833@gmail.com>
2022-01-15 14:52:17 +0600

    The search page is now dynamic.

--- search.php
+++ search.php
@@ -11,7 +11,43 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-
+    <section class="image-boxes-area">
+        <div class="container-fluid">
+            <div class="">
+                <h4 class="page-title">
+                    <?php
+                        printf( esc_html__( 'Are you looking for: %s', 'noonpost' ), '<span>' . get_search_query() . '</span>' );
+                    ?>
+                </h4>
+            </div>
+        </div>
+    </section>
+    <section class="post-area no-sidebar">
+        <div class="container-fluid">
+            <div class="post-area-inner">
+                <!-- Search Result Post -->
+                <div class="entry-posts two-column masonary-posts row">
+                    <?php
+                        while(have_posts()):
+                            the_post()
+                    ?>
+                    <div class="col-xl-3 col-lg-4 col-sm-6">
+                        <?php get_template_part('template-parts/content', 'search'); ?>
+                    </div>
+                    <?php
+                        endwhile;
+                    ?>
+                    <div class="col-12">
+                        <div class="text-center">
+                            <?php the_posts_pagination(  ); ?>
+                            <a href="#" class="load-more-btn">Load More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Post Area End ======-->
 </main>
 
 <?php
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,6 +10,42 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry-post">
+        <div class="entry-thumbnail">
+            <?php the_post_thumbnail('full') ?>
+        </div>
+        <div class="entry-content">
+            <h4 class="title">
+                <a href="<?php esc_url(the_permalink()); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h4>
+            <ul class="post-meta">
+                <li class="date">
+                    <a href="#"><?php the_date() ?></a>
+                </li>
+                <li class="categories">
+                    <?php
+                        $categories = get_the_category();
+                        $separator = ', ';
+                        $result = '';
+                        if ($categories) {
+                            foreach ($categories as $category) {
+                                $result .= '<a href="' . get_category_link($category->term_id) . '">' . $category -> cat_name . '</a>' . $separator ;
+                            }
+                            echo trim($result, $separator);     
+                        }
+                    ?>
+                </li>
+            </ul>
+            <p>
+                <?php the_excerpt() ?>
+            </p>
+            <a href="blog-details.html" class="read-more">
+                <?php echo esc_html__( 'Read More ', 'tukitwo') ?><i class="fas fa-long-arrow-right"></i>
+            </a>
+        </div>
+    </div>
     <header class="entry-header">
         <?php //the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
